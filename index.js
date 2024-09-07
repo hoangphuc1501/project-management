d2d1b400a5e4a56c44915daebb923a2d40febf00
hoangphuc1501 <katakurilancer@gmail.com>
2024-09-07 17:46:41 +0700

    Cài đặt PUG + Tạo views

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Trang chủ");
-});
+app.use(express.static('public')) // tìm thư mục tên là views
+app.set('views', './views') // templet engine sử dụng pug
+app.set('view engine', 'pug') // thiết lập thư mục chứ file tĩnh
 
-app.get("/products", () => {
-    res.send("trang danh sách sản phẩm");
+app.get('/', (req, res) => {
+    res.render('client/pages/home/index.pug')
+})
+
+app.get("/products", (req, res) => {
+    res.render('client/pages/products/index.pug')
 });
 
 app.listen(port, () => {
